d1f43a4b58f64775983b7867c6697456082961ae
Nynesh Reddy Gujjula <nyneshreddy@gmail.com>
2023-12-26 20:25:39 +0100

    Create page.tsx

---
+++ app/projects/[title]/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from 'next/navigation'
+import { allProjects } from 'contentlayer/generated'
+
+import { MDXComponent } from '@/components/molecules/mdx-component'
+
+type ParamsProps = {
+  title: string
+}
+
+async function getProjectFromParams(params: ParamsProps) {
+  const post = allProjects.find(post => post.title.toLowerCase() === params.title)
+  if (!post) null
+
+  return post
+}
+
+export async function generateStaticParams() {
+  return allProjects.map(post => ({
+    title: post.title.toLowerCase()
+  }))
+}
+
+export default async function ProjectDetail({ params }: { params: ParamsProps }) {
+  const about = await getProjectFromParams(params)
+  if (!about) return notFound()
+
+  return (
+    <MDXComponent
+      code={about.body.code}
+      className={
+        'prose-h2:text-foreground prose-h3:text-foreground prose-p:text-muted-foreground prose-a:text-foreground prose-li:text-muted-foreground prose-img:w-2/3 prose-img:mx-auto prose-strong:text-foreground prose-h4:text-foreground prose-a:no-underline'
+      }
+    />
+  )
+}
